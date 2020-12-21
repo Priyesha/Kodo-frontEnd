c40885c230280b0ac7b9112bfe47d790a1eaf40c
Priyesha Yadav <priyeshayadav9192@gmail.com>
2020-12-21 14:55:58 +0530

    UI state saved using routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { HomeService } from './home.service';
 import { GridData } from '../model/grid-data';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,32 +13,58 @@ export class HomeComponent implements OnInit {
   public data: GridData[] = [];
   private completeData: GridData[] = [];
   p: number = 1;
-  constructor(private homeService: HomeService) { }
+  public filterItem: string = '';
+  public sortingValue: string = '';
+  @ViewChild('searchInput') searchInput: ElementRef;
+  @ViewChild('sortByDropdown') sortByDropdown: ElementRef;
+  constructor(private homeService: HomeService, private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.data = this.homeService.getData();
     this.completeData = this.data;
+    this.route.queryParams.subscribe(params => {
+      this.filterItem = params['searchBy'];
+      this.sortingValue = params['sortBy'];
+    });
   }
 
-  search(event: Event) {
+  ngAfterViewInit() {
+    this.searchInput.nativeElement.value = this.filterItem;
+    this.sortByDropdown.nativeElement.value = this.sortingValue;
+    this.search(this.filterItem);
+    this.sort(this.sortingValue);
+  }
+
+  search(filterValue) {
     this.data = this.completeData;
-    const filterValue = (event.target as HTMLInputElement).value;
-    const filterItem = filterValue.trim().toLowerCase();
-    console.log(filterItem);
-    const searchTerms = filterItem.split(' ');
+    this.filterItem = filterValue.trim().toLowerCase();
+    console.log(this.filterItem);
+    const searchTerms = this.filterItem.split(' ');
     console.log(searchTerms);
-    if (filterItem && filterItem.length > 0) {
+    if (this.filterItem && this.filterItem.length > 0) {
       this.data = this.data.filter((item) => {
-        return item.name.toLowerCase().includes(filterItem) || item.description.toLowerCase().includes(filterItem)
+        return item.name.toLowerCase().includes(this.filterItem) || item.description.toLowerCase().includes(this.filterItem)
       });
     } else {
       this.data = this.data;
     }
+    this.updateRoute();
   }
 
   sort(value) {
-    if(value) {
-    this.data = this.data.sort((a,b) => a[value].localeCompare(b[value]));
-    } 
+    this.sortingValue = value;
+    if (this.sortingValue) {
+      this.data = this.data.sort((a, b) => a[value].localeCompare(b[value]));
+    }
+    this.updateRoute();
+  }
+
+  updateRoute() {
+    const params = { sortBy: this.sortingValue, searchBy: this.filterItem }
+    this.router.navigate(['.'], {
+      relativeTo: this.route,
+      queryParams: params
+    })
   }
 }
