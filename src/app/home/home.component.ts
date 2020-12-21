ec429d0fe090e35ebd940c16a4eec18d14046709
Priyesha Yadav <priyeshayadav9192@gmail.com>
2020-12-21 12:22:47 +0530

    search & sorting implemeneted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,34 @@ import { GridData } from '../model/grid-data';
 export class HomeComponent implements OnInit {
 
   public data: GridData[] = [];
+  private completeData: GridData[] = [];
   p: number = 1;
   constructor(private homeService: HomeService) { }
 
   ngOnInit(): void {
     this.data = this.homeService.getData();
+    this.completeData = this.data;
+  }
+
+  search(event: Event) {
+    this.data = this.completeData;
+    const filterValue = (event.target as HTMLInputElement).value;
+    const filterItem = filterValue.trim().toLowerCase();
+    console.log(filterItem);
+    const searchTerms = filterItem.split(' ');
+    console.log(searchTerms);
+    if (filterItem && filterItem.length > 0) {
+      this.data = this.data.filter((item) => {
+        return item.name.toLowerCase().includes(filterItem) || item.description.toLowerCase().includes(filterItem)
+      });
+    } else {
+      this.data = this.data;
+    }
+  }
+
+  sort(value) {
+    if(value) {
+    this.data = this.data.sort((a,b) => a[value].localeCompare(b[value]));
+    } 
   }
 }
